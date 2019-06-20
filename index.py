55e9b52f99eef4b559bd5ab29d8a4844a348870c
rigox <rigoc1994@gmail.com>
2019-06-19 23:18:26 -0400

    added more options and connection to api

--- index.py
+++ index.py
@@ -12,24 +12,28 @@ books_collection =[
 'description':"Vengance is fueld by time",
 'price':"4",
 'isbn':"3212342455",
+"ID":1
 },
 {'title':'the fountain Head',
 'author':"Ayn Rand",
 'description':"a mans selfishness is his best  strength",
 'price':'5',
 'isbn':"223213213",
+"ID":2
 },
 {'title':'anthem',
 'author':'ayn rand',
 'description':"a socialist destopia",
 'price':'5',
 'isbn':'4343242342',
+"ID":3
 },
 {'title':'the prince',
 'author':'niccolo machiavelli',
 'description':'lessons from the past are still relevant today',
 'price':'1',
 'isbn':'32234545435',
+"ID":4
 },
 ]
 
@@ -37,20 +41,34 @@ books_collection =[
 
 store =  Store(books_collection)
 
+def getBooks():
+    store.listBooks()
+
+
+
 def menu():
-     cprint("1--   View Items",'yellow')
-     cprint("2-- pruchase Items",'yellow')
-     cprint("3-- exit",'yellow')
+     print('\n')
+     cprint("\t\t\t1--  View Items",'yellow')
+     cprint("\t\t\t2--  Purchase Items",'yellow')
+     cprint("\t\t\t3--  View Cart","yellow")
+     cprint('\t\t\t4--  Checkout',"yellow")
+     cprint("\t\t\t5--  Exit",'yellow')
+     
 
 
          
 def main():
     counter  = 0
+    cprint("\t\t\tWELCOME TO LIBROS!!!!","magenta")
     while(counter!=4):
         menu()
         counter  =  int(input())
         if(counter==1):
-            cprint("a: {}".format("hello"),"red") 
+            getBooks()
+        elif(counter==2):
+            store.make_purchase()
+        elif(counter==3):
+            store.view_cart()
 
      
      
--- models/store.py
+++ models/store.py
@@ -1,18 +1,105 @@
 from .items import Book
 from termcolor import cprint
-
+from datetime import datetime
+import json
+import requests;
 
 class Store:
     def __init__(self,books):
         self.books = books
+        self.token =''
+        self.cart = []
+        self.Bearer= ''
 
     def listBooks(self):
         temp =  self.books
+        for x in temp:
+            cprint("ID: {}".format(x.get("ID")),'red')
+            cprint("Title: {}".format(x.get('title')),'magenta')
+            cprint("Author: {}".format(x.get('author')),'magenta')
+            cprint("Description: {}".format(x.get('description')),'magenta')
+            cprint("Price ${}.00".format(x.get('price')),'magenta')
+            print("------------\n")
+    
+    def make_purchase(self):
+
+       self.listBooks() 
+
+       choice =  input("Enter selection: ") 
+       print("\n")
+       check =  input("Is the selection with the ID :{} correct y/n? ".format(choice))
+       if(check=="y"):
+            book =  self.find_by_id(choice)
+            self.add_toCart(book)
+       
+    
+    def find_book(self,title):
+        temp  = self.books
+        for x in temp:
+            if(x.get('title') ==title):
+                return x
+    
+    def make_receipt(self,b):
+        transction_info  ={
+             'item':b.get('title'),
+             'price':b.get('price'),
+             'date': datetime.today().strftime('%Y-%m-%d')
+        }
+        return  transction_info
+    
+    def add_toCart(self,book):
+        self.cart.append(book)
+
+    def  payment_login(self):
+        cprint("Welcome to Banky your payment gateway","Purple")
+        username  = input("Enter your username: ")
+        password  = input("Enter you password: ")
+        self.username=username
+        res = requests.get("http://localhost:4000/Account/login",
+        params={'username':username,'password':password}
+        )
+        x ="biggies"
+        temp =  res.content
+        self.token =  temp.decode('utf-8')
+        self.Bearer = "Bearer {}".format(self.token)
+
+    def checkout(self):
+        items =  self.cart;
+        total=0
+        transaction_info=[]
+        for x in items:
+            total+= x.get('price')
+            transaction_info.append(self.make_receipt(x))
+        
+        res =  requests.post("http://localhost:4000/Account/purchase",
+        params={'info':json.dumps(transaction_info),'amount':total,'username':str(self.username)},
+        headers={"Authorization":self.Bearer}
+        )
+
+        if res.status_code == 200:
+            print("Purchase completed")
+        else:
+            print("Error")
+    
+    def find_by_id(self,ID):
+        books =  self.books
 
         for x in books:
-            cprint("Title: {}".format(x.title),'magenta')
-            cprint("Title: {}".format(x.author),'magenta')
-            cprint("Title: {}".format(x.description),'magenta')
-            cprint("Title: {}".format(x.price),'magenta')
-            print("------------\n")
+            if(x.get("ID")==int(ID)):
+                return x
+    
+    def view_cart(self):
+        items =  self.cart
+        total = 0
+        for x in items:
+            cprint(x.get('title'),"yellow")
+            total+=int(x.get('price'))
+        
+        cprint("Total: ${}".format(total),"magenta")
+
+
+            
+
+
+
 
